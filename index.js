46ce6451312d51da31b3068edf3ed1d0e290824a
abhipatel1369 <72403818+abhipatel1369@users.noreply.github.com>
2023-05-19 21:49:41 +0530

    Work in Progress

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let scores = new Array(0,0,1,true,false,true);
+
+const index = scores.indexOf(true);
+console.log(Array.isArray(scores));
+
+// console.log(scores);
+// console.log(scores.length);
+
+//Basic Operation on Array
+
+// scores.push("Hello","World");
+
+// scores.unshift("World");
+// console.log(scores);
+
+
+// Javascript Object
+// let person = {
+//     name : "Abhi",
+//     surname:"Patel"
+// }
+
+// const abhi = person;
+// abhi.name = "Abhi Patel"
+// abhi.surname = "";
+
+// console.log(abhi);
+// person.address = "Ahmedabad";
+// delete person.name;
+// console.log('xyz' in person);
+// let error ="404 Error Occured";
+// let result = 'A' < 'B';
+// console.log(result);
+// console.log(error[0]);
